f2b2b132dcb8985ef75e69491a284020a79168c7
Shavin Croos <t.s.croos@student.reading.ac.uk>
2021-04-14 20:10:58 +0100

    Added DecorativeBlockUpdate and InanimateBlockGraphics
    Added DecorativeBlockUpdate and InanimateBlockGraphics classes.

---
+++ Mineplex/app/src/main/java/com/shavin/androidstudiogamecoursework/DecorativeBlockUpdateComponent.java
@@ -0,0 +1,9 @@
+package com.shavin.androidstudiogamecoursework;
+
+class DecorativeBlockUpdateComponent implements UpdateComponent {
+    @Override
+    public void update(long fps, Transform t, Transform playerTransform) {
+        // Do nothing
+        // Not even set a collider
+    }
+}
